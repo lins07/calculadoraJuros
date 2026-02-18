c87e8c44eaeb3abd941bd3f6a5eb698aa0733b72
lins07 <estudoslins59@gmail.com>
2026-02-18 18:09:39 -0300

    .

--- demo/src/main/java/com/calculadora/demo/controller/UsuarioController.java
+++ demo/src/main/java/com/calculadora/demo/controller/UsuarioController.java
@@ -3,8 +3,6 @@ package com.calculadora.demo.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
