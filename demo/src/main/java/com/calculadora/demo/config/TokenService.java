b56abb137fa896065876dbd8b207e2e3e5ea9e15
lins07 <estudoslins59@gmail.com>
2026-02-18 17:16:43 -0300

    feat: implement JWT token generation and validation service

---
+++ demo/src/main/java/com/calculadora/demo/config/TokenService.java
@@ -0,0 +1,57 @@
+package com.calculadora.demo.config;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.calculadora.demo.model.Usuario;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Usuario usuario) {
+
+        try {
+    Algorithm algorithm = Algorithm.HMAC256(secret);
+
+    String token = JWT.create()
+            .withIssuer("CalculadoraDeJuros")
+            .withSubject(usuario.getEmail())
+            .withExpiresAt(genExpiresAt())
+            .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException exception) {
+        throw new RuntimeException("Error while generating token", exception);
+        
+        } 
+    }
+
+    public String ValidateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("CalculadoraDeJuros")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    private Instant genExpiresAt() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
